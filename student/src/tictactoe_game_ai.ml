07450f87ab4ff37ff67c384c1ff3fa3755a4aef2
Makda Dejene <makdadejene@gmail.com>
2023-06-29 20:54:48 +0000

    Minimax

--- student/src/tictactoe_game_ai.ml
+++ student/src/tictactoe_game_ai.ml
@@ -88,14 +88,62 @@ let score
   ~(pieces : Piece.t Position.Map.t)
   : float
   =
-  (* Tic_tac_toe_exercises_lib.evaluate ~me ~game_kind ~pieces *)
-  ignore me;
-  ignore game_kind;
-  ignore pieces;
-  0.0
+  let curr = Tic_tac_toe_exercises_lib.evaluate ~game_kind ~pieces in
+  match curr with
+  | Tic_tac_toe_exercises_lib.Evaluation.Game_over { winner = Some x } ->
+    if Piece.equal x me then Float.infinity else Float.neg_infinity
+  | _ -> 0.0
 ;;
 
 let _ = score
+
+let rec minimax
+  ~(me : Piece.t)
+  ~(game_kind : Game_kind.t)
+  ~(game_status : Game_status.t)
+  ~(pieces : Piece.t Position.Map.t)
+  ~(curr_score : Position.t list)
+  ~(depth : int)
+  : ((float), (Position.t))
+  =
+  let curr = Tic_tac_toe_exercises_lib.evaluate ~game_kind ~pieces in
+  match curr with
+  | Tic_tac_toe_exercises_lib.Evaluation.Game_over { winner = Some x } ->
+    score ~me ~game_kind ~pieces
+  | _ ->
+    if depth = 0
+    then score ~me ~game_kind ~pieces
+    else (
+      let avail_moves =
+        Tic_tac_toe_exercises_lib.available_moves ~game_kind ~pieces
+      in
+      let curr_score =
+        List.map avail_moves ~f:(fun position ->
+          let new_pieces = Map.set pieces ~key:position ~data:me in
+          minimax
+            ~me: (Piece.flip me)
+            ~game_kind
+            ~game_status
+            ~pieces:new_pieces
+            ~curr_score
+            ~depth:(depth - 1))
+      in
+      if Game_status.equal game_status (Game_status.Turn_of me)
+      then (
+        match List.max_elt curr_score ~compare:Float.compare with
+        | Some p -> p
+        | None -> failwith "No")
+      else (
+        match List.min_elt curr_score ~compare:Float.compare with
+        | Some p -> p
+        | None -> failwith "No"))
+;;
+
+(* - pass in me, game_kind, pieces, current Player - check if game is over:
+   match with score - if Player = me then create a variable called val = -
+   infin then recursively go through all available moves - - if Player =
+   other player then create a variable call val = + infin then recursively go
+   through available moves to determine value *)
 
 (* [compute_next_move] is your Game AI's function.
 
@@ -110,13 +158,18 @@ let compute_next_move ~(me : Piece.t) ~(game_state : Game_state.t)
   : Position.t
   =
   ignore me;
-  let new_move =
-    pick_winning_move_or_block_if_possible_strategy
+  
+     match minimax
       ~me
       ~game_kind:game_state.game_kind
+      ~game_status:game_state.game_status
       ~pieces:game_state.pieces
-  in
-  new_move
+      ~curr_score: []
+      ~depth:6 with 
+      | Float.infinity -> 
+      | Float.neg_infinity ->
+      | _ ->
+
 ;;
 
 (* ignore game_state; { Position.row = 0; column = 0 } *)
